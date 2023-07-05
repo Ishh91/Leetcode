d5ca30361e40f68ad2f441a3c4ab22a3e0c39fee
Ishant Sharma <80956605+Ishh91@users.noreply.github.com>
2023-07-05 07:40:56 +0530

    Added solution - LeetHub

---
+++ LRU Cache - GFG/lru-cache.cpp
@@ -0,0 +1,125 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// design the class in the most optimal way
+
+class LRUCache
+{
+    private:
+
+    public:
+    class node {
+      public:
+        int key;
+      int val;
+      node * next;
+      node * prev;
+      node(int _key, int _val) {
+        key = _key;
+        val = _val;
+      }
+    };
+  node * head = new node(-1, -1);
+  node * tail = new node(-1, -1);
+  int cap;
+  unordered_map < int, node * > m;
+  void addnode(node * newnode) {
+    node * temp = head -> next;
+    newnode -> next = temp;
+    newnode -> prev = head;
+    head -> next = newnode;
+    temp -> prev = newnode;
+  }
+
+  void deletenode(node * delnode) {
+    node * delprev = delnode -> prev;
+    node * delnext = delnode -> next;
+    delprev -> next = delnext;
+    delnext -> prev = delprev;
+  }
+    LRUCache(int capacity)
+    {
+        cap = capacity;
+        head -> next = tail;
+        tail -> prev = head;
+        // Write your code here
+    }
+    //Function to return value corresponding to the key.
+    int GET(int key_)
+    {
+        if (m.find(key_) != m.end()) {
+      node * resnode = m[key_];
+      int res = resnode -> val;
+      m.erase(key_);
+      deletenode(resnode);
+      addnode(resnode);
+      m[key_] = head -> next;
+      return res;
+    }
+
+    return -1;
+        // your code here
+    }
+    
+    //Function for storing key-value pair.
+    void SET(int key_, int value)
+    {
+        if (m.find(key_) != m.end()) {
+      node * existingnode = m[key_];
+      m.erase(key_);
+      deletenode(existingnode);
+    }
+    if (m.size() == cap) {
+      m.erase(tail -> prev -> key);
+      deletenode(tail -> prev);
+    }
+
+    addnode(new node(key_, value));
+    m[key_] = head -> next;
+        // your code here   
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int capacity;
+        cin >> capacity;
+        LRUCache *cache = new LRUCache(capacity);
+        
+        int queries;
+        cin >> queries;
+        while (queries--)
+        {
+            string q;
+            cin >> q;
+            if (q == "SET")
+            {
+                int key;
+                cin >> key;
+                int value;
+                cin >> value;
+                cache->SET(key, value);
+            }
+            else
+            {
+                int key;
+                cin >> key;
+                cout << cache->GET(key) << " ";
+            }
+        }
+        cout << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
